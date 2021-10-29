2ee672e513ed3c0d355dd066c6b0b2012f07def8
Walied Rashied <w.rashied@icloud.com>
2021-10-29 10:25:21 +0200

    add samples.http

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+/* const country = {
+  name: 'Germany',
+  capital: 'Berlin',
+  population: '83129285'
+} */
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
