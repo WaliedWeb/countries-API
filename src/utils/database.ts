796646eed61306763b6a4b12e16bfbc012d5b554
PReichetanz <paul.reichetanzgmx.de>
2021-10-29 11:35:47 +0200

    Fix typo in database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 let client;
 
-export async function connectDatabase(url) {
+export async function connectDatabase(url: string) {
   client = new MongoClient(url);
   await client.connect();
 }
